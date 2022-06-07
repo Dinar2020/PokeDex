6a2fc3f9cd439516fa7d50c6d75c08cbcd122ffd
Dinario Rebouças <dinario123@gmail.com>
2022-06-07 19:48:33 -0300

    Consumindo API para pegar o nome e o tipo do pokemon e mudanças no html para inserí-los

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ console.log(div)
 const page = document.querySelector('#pokedex-page')
 // page.appendChild(div)
 
-fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
+fetch('https://pokeapi.co/api/v2/pokemon?limit=150&offset=0')
     .then(response =>{
+    console.log(response) 
     return response.json()
-    }) 
+    })
+    
     .then( async data => {
         const box = document.querySelector('#pokemon-box')
         page.innerHTML = ''
         // console.log(box)
-        // // console.log(data.results)
+        console.log(data)
 
 
         for(let i=0 ; i < data.results.length; i++) {
@@ -25,11 +27,28 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
             const image = await pokemonImage.json()
             box.querySelector('#pokemon-img').src= image.sprites.front_default
             console.log(image.sprites.front_default)
+            const pokemonInfo = await fetch(`https://pokeapi.co/api/v2/pokemon/${i+1}`)
+            const info = await pokemonInfo.json()
+            console.log(info)
+            box.querySelector('#pokemon-type').innerHTML = info.types[0].type.name
+            box.querySelector('#pokemon-type').style.textTransform="capitalize"
             page.innerHTML += box.outerHTML
+            // const pokemonType = await fetch('https://pokeapi.co/api/v2/type/')
+            // const type = await pokemonType.json()
+            // console.log(type)
+            // box.querySelector('#pokemon-type').innerHTML = type.results[i].name
+            
+            // 'if pokemon.types.type.name ==                       '
+
+            // if box.querySelector('#pokemon-type').innerHTML = type.results[i].name
             
             // console.log(data.results[i].name)
         }
 
         
     })
+
+
+
+    // for (let i=0; i < )
 
